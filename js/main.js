5647e0724b7efcd470085c06da56246d6428957c
premsharma0123 <prem@massmancybergeeks.com>
2021-02-24 18:37:42 +0530

    commit

---
+++ js/main.js
@@ -0,0 +1,54 @@
+$(document).ready(function(){
+
+      // add & Remove class
+   $(".List01").click(function () {
+    if(!$(this).hasClass('List_active'))
+    {    
+        $(".List01.List_active").removeClass("List_active");
+        $(this).addClass("List_active");        
+    }
+  });
+
+
+// ----price---list-----//
+$(".a1").click(function(){
+    $("#BTC").show();
+    $("#BCH,#ETC,#ZEC,#XMR,#LTC,#ETH,#DASH").hide();
+});
+ 
+  $(".a2").click(function(){
+      $("#BCH").show();
+      $("#BTC,#ETC,#ZEC,#XMR,#LTC,#ETH,#DASH").hide( );
+  })
+
+  $(".a3").click(function(){
+    $("#ETC").show();
+    $("#BTC,#BCH,#ZEC,#XMR,#LTC,#ETH,#DASH").hide( );
+})
+$(".a4").click(function(){
+    $("#ZEC").show();
+    $("#BTC,#BCH,#ETC,#XMR,#LTC,#ETH,#DASH").hide( );
+})
+$(".a5").click(function(){
+    $("#XMR").show();
+    $("#BTC,#BCH,#ETC,#LTC,#ZEC,#ETH,#DASH").hide( );
+})
+$(".a6").click(function(){
+    $("#LTC").show();
+    $("#BTC,#BCH,#ETC,#XMR,#ZEC,#ETH,#DASH").hide( );
+})
+$(".a7").click(function(){
+    $("#ETH").show();
+    $("#BTC,#BCH,#ETC,#XMR,#ZEC,#LTC,#DASH").hide( );
+})
+$(".a8").click(function(){
+    $("#DASH").show();
+    $("#BTC,#BCH,#ETC,#XMR,#ZEC,#LTC,#ETH").hide( );
+})
+
+// price--list--end--here---//
+
+
+})
+  
+  
