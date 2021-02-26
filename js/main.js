bb854f424442b93dc98619502757d4eaf5865d5c
premsharma0123 <prem@massmancybergeeks.com>
2021-02-26 18:32:10 +0530

    commit

--- js/main.js
+++ js/main.js
@@ -57,32 +57,47 @@ $(".a1").click(function(){
       $("#BTC,#ETC,#ZEC,#XMR,#LTC,#ETH,#DASH").hide( );
   })
 
-  $(".a3").click(function(){
-    $("#ETC").show();
-    $("#BTC,#BCH,#ZEC,#XMR,#LTC,#ETH,#DASH").hide( );
-})
-$(".a4").click(function(){
-    $("#ZEC").show();
-    $("#BTC,#BCH,#ETC,#XMR,#LTC,#ETH,#DASH").hide( );
-})
-$(".a5").click(function(){
-    $("#XMR").show();
-    $("#BTC,#BCH,#ETC,#LTC,#ZEC,#ETH,#DASH").hide( );
-})
-$(".a6").click(function(){
-    $("#LTC").show();
-    $("#BTC,#BCH,#ETC,#XMR,#ZEC,#ETH,#DASH").hide( );
-})
-$(".a7").click(function(){
-    $("#ETH").show();
-    $("#BTC,#BCH,#ETC,#XMR,#ZEC,#LTC,#DASH").hide( );
-})
-$(".a8").click(function(){
-    $("#DASH").show();
-    $("#BTC,#BCH,#ETC,#XMR,#ZEC,#LTC,#ETH").hide( );
-})
+//   $(".a3").click(function(){
+//     $("#ETC").show();
+//     $("#BTC,#BCH,#ZEC,#XMR,#LTC,#ETH,#DASH").hide( );
+// })
+// $(".a4").click(function(){
+//     $("#ZEC").show();
+//     $("#BTC,#BCH,#ETC,#XMR,#LTC,#ETH,#DASH").hide( );
+// })
+// $(".a5").click(function(){
+//     $("#XMR").show();
+//     $("#BTC,#BCH,#ETC,#LTC,#ZEC,#ETH,#DASH").hide( );
+// })
+// $(".a6").click(function(){
+//     $("#LTC").show();
+//     $("#BTC,#BCH,#ETC,#XMR,#ZEC,#ETH,#DASH").hide( );
+// })
+// $(".a7").click(function(){
+//     $("#ETH").show();
+//     $("#BTC,#BCH,#ETC,#XMR,#ZEC,#LTC,#DASH").hide( );
+// })
+// $(".a8").click(function(){
+//     $("#DASH").show();
+//     $("#BTC,#BCH,#ETC,#XMR,#ZEC,#LTC,#ETH").hide( );
+// })
 
 // price--list--end--here---//
+$('.Currency-item1').click(function(){
+   $('#List2').hide(( "clip", {direction: "vertical"}, 1000 ))
+});
+
+$('.Currency-item2').click(function(){
+    $('#List1').hide(( "clip", {direction: "vertical"}, 1000 ))
+ });
+
+
+//  ---plans---button---//
+$('.P-Btn').click(function(){
+    $('.srv4').toggle();
+})
+
+
 
 
 })
@@ -92,8 +107,8 @@ function openList(ListName) {
     var i;
     var x = document.getElementsByClassName("quantity-wrp");
     for (i = 0; i < x.length; i++) {
-      x[i].style.display = "none";  
+        x[i].style.display = "none";  
     }
     document.getElementById(ListName).style.display = "block";  
-  }
-  //-----temp--list---//
+}
+//-----temp--list---//
